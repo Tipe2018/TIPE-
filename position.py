3ed6bf0164e08d04c7f3f089ef3984ad74ebae7b
Caralg <35164444+Caralg@users.noreply.github.com>
2018-05-22 08:40:33 +0200

    Add files via upload

---
+++ position.py
@@ -0,0 +1,49 @@
+
+'Intégrations des valeurs pour obtenir la position'
+import numpy as np
+#Pour le graphe 3D
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as pl
+
+def position (Lx,Ly,Lz,fin,n): 
+#L liste des accélérations,de t=0 à t=fin, n=taille de L
+    vitesseX=[]
+    vitesseY=[]
+    vitesseZ=[]
+    
+    positionX=[]
+    positionY=[]
+    positionZ=[]
+    
+    'méthode des trapèzes'
+    tau=fin/n # tau=(b-a)/n avec a=0, b=fin
+    
+    for k in range (n-1):
+        vx=tau*(Lx[k]+Lx[k+1])/2 # vitesse suivant x
+        vitesseX.append(vx)
+        
+        vy=tau*(Ly[k]+Ly[k+1])/2 # vitesse suivant x
+        vitesseY.append(vy)
+        
+        vz=tau*(Lz[k]+Lz[k+1])/2 # vitesse suivant x
+        vitesseZ.append(vz)
+    
+    for i in range (n-2):
+        px=tau*(vitesseX[i]+vitesseX[i+1])/2
+        positionX.append(px)
+        
+        py=tau*(vitesseY[i]+vitesseY[i+1])/2
+        positionY.append(py)
+        
+        pz=tau*(vitesseZ[i]+vitesseZ[i+1])/2
+        positionZ.append(pz)
+    return positionX, positionY, positionZ
+    
+           
+    #Graphe de la position du ballon'
+    fig=pl.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot(positionX, positionY, positionZ)
+    pl.show()
