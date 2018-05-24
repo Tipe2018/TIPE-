1a51039d8fe1ebdb9e6f88701e35ef3a29f82875
Caralg <35164444+Caralg@users.noreply.github.com>
2018-05-24 12:41:31 +0000

    Add files via upload

--- fonctions.py
+++ fonctions.py
@@ -226,7 +226,7 @@ def conversion(l): # convertit liste en csv
 
     file=open("valeurs.csv",'w')
     for i in range (len (l[0])-1):
-        file.write(str(l[0][i],l[1][i],l[2][i],l[3][i],l[4][i],l[5][i],l[6][i]))
+        file.write(str([l[0][i],l[1][i],l[2][i],l[3][i],l[4][i],l[5][i],l[6][i]]))
     ecriture=csv.writer(file,dialect='excel',delimiter=';')
     ecriture.writerows([l]) 
     file.close()
