2a515031a2661ae083409e80960a903feee96c1c
Caralg <35164444+Caralg@users.noreply.github.com>
2018-05-25 15:14:44 +0200

    ok

--- position.py
+++ position.py
@@ -12,9 +12,9 @@ file=open('valeurs.csv','r')
 file.readline() #lis la 1e lign( celle des textes)
 Lx,Ly,Lz,T=[],[],[],[]
 for ligne in file:
-    Lx.apppend(ligne[1])
-    Ly.apppend(ligne[2])
-    Lz.apppend(ligne[3])
+    Lx.append(ligne[1])
+    Ly.append(ligne[2])
+    Lz.append(ligne[3])
     T.append(ligne[0])
 def position (Lx,Ly,Lz,fin,n): 
 #L liste des accélérations,de t=0 à t=fin, n=taille de L
@@ -26,7 +26,7 @@ def position (Lx,Ly,Lz,fin,n):
     positionY=[]
     positionZ=[]
     
-    'méthode des trapèzes'
+    #'méthode des trapèzes'
     tau=fin/n # tau=(b-a)/n avec a=0, b=fin
     
     for k in range (0,n-1):
@@ -56,7 +56,7 @@ def position (Lx,Ly,Lz,fin,n):
     ax = fig.gca(projection='3d')
     ax.plot(positionX, positionY, positionZ)
     pl.show()
-position (Lx,Ly,Lz,T[len(T)-1],len(T))
+position (Lx,Ly,Lz,T[-1],len(T))
 
 ##Test:
 #t=np.linspace (0,5,20)
